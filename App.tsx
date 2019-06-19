8e8c30b607ecf5089b3c533d95c66401a550d26c
Yaron Malin <yaronmln@google.com>
2019-06-19 23:18:50 +0300

    Basic app

--- App.tsx
+++ App.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+
+    state = {
+        text: "Press a button to start"
+    };
+
+    changeText = (newText: string) => this.setState({
+        text: newText
+    });
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                    {this.state.text}
+                </Text>
+
+                <TouchableOpacity
+                    onPress={() => this.changeText("First button pressed!")}
+                    style={styles.button}
+                    testID="FirstButton"
+                >
+                    <Text>First button</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => this.changeText("Second button pressed!")}
+                    style={styles.button}
+                    testID="SecondButton"
+                >
+                    <Text>Second button</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {},
+    button: {}
 });
