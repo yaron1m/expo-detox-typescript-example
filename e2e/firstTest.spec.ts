8f6e18872f00741fcf8b91f6e858fbcef8983738
Yaron Malin <yaronmln@google.com>
2019-06-22 18:40:02 +0300

    Add another test

--- e2e/firstTest.spec.ts
+++ e2e/firstTest.spec.ts
@@ -10,4 +10,18 @@ describe('Authentication tests', () => {
         await expect(element(by.id("MainText"))).toExist();
         await expect(element(by.label("Press a button to start"))).toBeVisible();
     });
+
+    it('should change the text when buttons are pressed', async () => {
+        await expect(element(by.id("MainText"))).toExist();
+        await expect(element(by.label("Press a button to start"))).toBeVisible();
+
+        await element(by.id("FirstButton")).tap();
+        await expect(element(by.label("First button pressed!"))).toBeVisible();
+
+        await element(by.id("SecondButton")).tap();
+        await expect(element(by.label("Second button pressed!"))).toBeVisible();
+
+        await element(by.id("FirstButton")).tap();
+        await expect(element(by.label("First button pressed!"))).toBeVisible();
+    });
 });
